03b1daf94cd94cdecabe8e2aac440fba9b14de3c
Paul Llamoja <palls257@gmail.com>
2025-08-06 19:39:15 -0500

    Add a feedback widget to the vendor

---
+++ Source/ItemVendorDemo/Components/FeedbackWidgetComponent.cpp
@@ -0,0 +1,86 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FeedbackWidgetComponent.h"
+
+#include "Components/BoxComponent.h"
+#include "Components/WidgetComponent.h"
+
+
+UFeedbackWidgetComponent::UFeedbackWidgetComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UFeedbackWidgetComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	UnbindFromBoxComponentEvents();
+	
+	Super::EndPlay(EndPlayReason);
+}
+
+
+void UFeedbackWidgetComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetFeedbackWidget();
+	BindToBoxComponentEvents();
+}
+
+void UFeedbackWidgetComponent::OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (IsValid(OtherActor) && IsValid(FeedbackWidget) && OtherActor->GetLocalRole() == ROLE_AutonomousProxy)
+	{
+		FeedbackWidget->SetVisibility(ESlateVisibility::Visible);
+	}
+}
+
+void UFeedbackWidgetComponent::OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (IsValid(OtherActor) && IsValid(FeedbackWidget) && OtherActor->GetLocalRole() == ROLE_AutonomousProxy)
+	{
+		FeedbackWidget->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
+
+void UFeedbackWidgetComponent::SetFeedbackWidget()
+{
+	UWidgetComponent* FeedbackWidgetComponent = GetOwner()->FindComponentByTag<UWidgetComponent>(FeedbackWidgetTag);
+	if (!IsValid(FeedbackWidgetComponent))
+	{
+		return;
+	}
+	
+	FeedbackWidget = FeedbackWidgetComponent->GetUserWidgetObject();
+	if (!IsValid(FeedbackWidget))
+	{
+		return;
+	}
+
+	FeedbackWidget->SetVisibility(ESlateVisibility::Hidden);
+}
+
+
+void UFeedbackWidgetComponent::BindToBoxComponentEvents()
+{
+	BoxCollisionComponent = GetOwner()->FindComponentByClass<UBoxComponent>();
+	if (IsValid(BoxCollisionComponent))
+	{
+		BoxCollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &UFeedbackWidgetComponent::OnBoxBeginOverlap);
+		BoxCollisionComponent->OnComponentEndOverlap.AddDynamic(this, &UFeedbackWidgetComponent::OnBoxEndOverlap);
+	}	
+}
+
+void UFeedbackWidgetComponent::UnbindFromBoxComponentEvents()
+{
+	if (IsValid(BoxCollisionComponent))
+	{
+		BoxCollisionComponent->OnComponentBeginOverlap.RemoveDynamic(this, &UFeedbackWidgetComponent::OnBoxBeginOverlap);
+		BoxCollisionComponent->OnComponentEndOverlap.RemoveDynamic(this, &UFeedbackWidgetComponent::OnBoxEndOverlap);
+		BoxCollisionComponent = nullptr;
+	}
+}
+
---
+++ Source/ItemVendorDemo/Components/FeedbackWidgetComponent.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "FeedbackWidgetComponent.generated.h"
+
+
+class UBoxComponent;
+class UWidgetComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ITEMVENDORDEMO_API UFeedbackWidgetComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFeedbackWidgetComponent();
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	FName FeedbackWidgetTag = "feedback-widget";
+	
+	UUserWidget* FeedbackWidget = nullptr;
+
+	UBoxComponent* BoxCollisionComponent = nullptr;
+
+private:
+	UFUNCTION()
+	void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+							   UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+							   const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+							 UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+	void SetFeedbackWidget();
+
+	void BindToBoxComponentEvents();
+
+	void UnbindFromBoxComponentEvents();
+};
